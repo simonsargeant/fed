2a8195f482d7345c31bd015e6b94c2c4b5bff164
Simon Sargeant <simonashleysargeant@gmail.com>
2021-03-05 21:49:12 +0100

    Fix docker tag syntax

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,7 +9,7 @@ GOOS=linux GOARCH=amd64 go build -o ./bin/fed ./cmd/fed
 docker login "${DOCKER_HOST}" -u "${DOCKER_USER}" -p "${DOCKER_TOKEN}"
 
 docker build "$PWD" -t "simonsargeant/fed:${IMAGE_TAG}"
-docker tag "simonsargeant/fed:${IMAGE_TAG}" -t "simonsargeant/fed:latest"
+docker tag "simonsargeant/fed:${IMAGE_TAG}" "simonsargeant/fed:latest"
 
 docker push "simonsargeant/fed:${IMAGE_TAG}"
 docker push "simonsargeant/fed:latest"
